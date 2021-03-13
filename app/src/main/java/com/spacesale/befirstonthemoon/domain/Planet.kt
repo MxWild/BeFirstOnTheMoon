91038a47546a0be3ba5cf22072c9f6c9c793d17b
Sviat98 <bashkevich.98@gmail.com>
2021-03-13 10:04:35 +0300

    edited Planet domain for details screen

--- app/src/main/java/com/spacesale/befirstonthemoon/domain/Planet.kt
+++ app/src/main/java/com/spacesale/befirstonthemoon/domain/Planet.kt
@@ -1,10 +1,14 @@
 package com.spacesale.befirstonthemoon.domain
 
+import androidx.annotation.DrawableRes
+
 data class Planet (
     val id: Int,
     val name: String,
-    val mainPoster: String,
-    val detailPoster: String,
+    @DrawableRes
+    val mainPoster: Int,
+    @DrawableRes
+    val detailPoster: Int,
     val texture: String,
     val description: String,
     val atmosphere: String,
