ebca60f618aaefb35b4368fdcf28941452ee3bd0
mxwild <mxwild@gmail.com>
2021-03-13 21:15:36 +0300

    Добавил полигон

--- app/src/main/java/com/spacesale/befirstonthemoon/view/globe/GlobeFragment.kt
+++ app/src/main/java/com/spacesale/befirstonthemoon/view/globe/GlobeFragment.kt
@@ -9,15 +9,22 @@ import androidx.fragment.app.Fragment
 import com.spacesale.befirstonthemoon.R
 import com.spacesale.befirstonthemoon.databinding.FragmentGlobeBinding
 import com.spacesale.befirstonthemoon.view.profile.ProfileFragment
+import gov.nasa.worldwind.WorldWind
 import gov.nasa.worldwind.WorldWindow
+import gov.nasa.worldwind.geom.Position
 import gov.nasa.worldwind.layer.BackgroundLayer
+import gov.nasa.worldwind.layer.RenderableLayer
 import gov.nasa.worldwind.render.ImageSource
+import gov.nasa.worldwind.shape.Polygon
+import gov.nasa.worldwind.shape.ShapeAttributes
 import org.koin.android.viewmodel.ext.android.viewModel
 import kotlin.properties.Delegates
+
 
 class GlobeFragment : Fragment() {
 
     private val viewModel: GlobeViewModel by viewModel()
+    var layer = RenderableLayer()
 
     private lateinit var wwd: WorldWindow
     private var planetId by Delegates.notNull<Int>()
@@ -45,6 +52,8 @@ class GlobeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        showPolygons()
+
         viewModel.planet.observe(viewLifecycleOwner) {
             showGlobe(planetId)
         }
@@ -66,7 +75,8 @@ class GlobeFragment : Fragment() {
                 .makeText(
                     context,
                     String.format(getString(R.string.buy_toast), planetId.toString()),
-                    Toast.LENGTH_LONG)
+                    Toast.LENGTH_LONG
+                )
                 .show()
         }
     }
@@ -86,6 +96,29 @@ class GlobeFragment : Fragment() {
         _binding = null
     }
 
+    private fun showPolygons() {
+        val positions = listOf<Position>(
+            Position.fromDegrees(40.0, -105.0, 0.0),
+            Position.fromDegrees(45.0, -110.0, 0.0),
+            Position.fromDegrees(50.0, -100.0, 0.0),
+            Position.fromDegrees(45.0, -90.0, 0.0),
+            Position.fromDegrees(40.0, -95.0, 0.0)
+        )
+        val poly = Polygon(positions)
+
+        val commonAttrs = ShapeAttributes()
+        commonAttrs.interiorColor[1.0f, 1.0f, 0.0f] = 0.5f
+        commonAttrs.outlineColor[0.0f, 0.0f, 0.0f] = 1.0f
+        commonAttrs.outlineWidth = 3f
+
+        poly.altitudeMode = WorldWind.CLAMP_TO_GROUND
+        poly.isFollowTerrain = true
+        poly.pathType = WorldWind.LINEAR
+        poly.attributes = ShapeAttributes(commonAttrs)
+
+        layer.addRenderable(poly)
+    }
+
     private fun showGlobe(planetId: Int) {
         viewModel.loadPlanetInfo(planetId)
         binding.globe.addView(createWorldWindow())
@@ -97,6 +130,7 @@ class GlobeFragment : Fragment() {
         }
 //        wwd.setBackgroundResource(R.drawable.background_stars)
         wwd.layers.addLayer(BackgroundLayer(moon, null))
+        wwd.layers.addLayer(layer)
         return wwd
     }
 
